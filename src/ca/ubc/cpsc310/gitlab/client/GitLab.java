9664594418f984097d715324b852897aaca95955
Maxwell Gordon <maxjgordon@hotmail.com>
2013-10-04 11:58:26 -0700

    creating a conflict

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -42,9 +42,8 @@ public class GitLab implements EntryPoint {
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
 	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
+	int x = 3;
+	final String error = "errors";
 
 //<<<<<<< HEAD
 	private final FlexTable flexTable = new FlexTable();
